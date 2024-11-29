20a77320dbff2177e054de1867464065767fdba6
Maaz Qureshi <maazx.qureshi@mail.utoronto.ca>
2024-11-28 22:07:19 -0500

    Input loop for simple CLI working. Can begin debugging + demo development

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,11 @@
-use std::io;
+use std::io::{self, BufRead};
 use std::io::Write;
+use std::thread;
 use gamesync_client::client::{GameSyncClient, MessageHandler};
 use gamesync_client::lobby::{GameMode, Lobby, LobbyParams, Player, Region, Visibility};
 use gamesync_client::server_events::ServerEvent;
 use uuid::Uuid;
+use tokio::task;
 
 #[derive(Clone)]
 pub struct MyMessageHandler {
@@ -26,7 +28,13 @@ impl MessageHandler for MyMessageHandler {
             ServerEvent::UserMessage(id, msg) => {
                 let message = format!("[{}]: {}",id,  msg);
                 println!("[{}]: {}",id,  msg);
-            }
+            },
+            ServerEvent::LobbyDeleted(id) => {
+                println!("Lobby {id} successfully deleted");
+            },
+            ServerEvent::LobbyJoined(id) => {
+                println!("New player joined in lobby {id}");
+            },
             // Handle other message types
             _ => {}
         }
@@ -52,6 +60,7 @@ async fn main() {
         client: client.clone(),
         players: Vec::new()
     };
+
     client.register_callback(handler).expect("Error registering game callback");
 
     let id = client.get_self().unwrap();
@@ -66,30 +75,31 @@ async fn main() {
         mode: GameMode::Casual
     };
 
-
     let result = client.create_lobby(id, lobby_params);
 
-    // ATTEMPT TO PRINT STATUS WHILE READING INPUT - DOESN'T WORK
-    // loop {
-    //     // Read user input
-    //     let input = get_user_input();
-    //
-    //     let parts: Vec<&str> = input.split_whitespace().collect();
-    //
-    //     let result = match parts.as_slice() {
-    //         ["broadcast", msg] => Ok(client.send_to_all_clients(msg.to_string()).expect("Failed to broadcast")),
-    //         ["sendto", recipient, msg] => Ok(client.send_to(Uuid::parse_str(recipient).unwrap(), msg.to_string()).expect("Failed to send message")),
-    //         ["create_lobby", player_id, lobby_params] => Ok(client.create_lobby(Uuid::parse_str(player_id).unwrap(), serde_json::from_str::<LobbyParams>(lobby_params).unwrap()).expect("Failed to send message")),
-    //         ["join_lobby", player_id, lobby_id] => Ok(client.join_lobby(Uuid::parse_str(player_id).unwrap(), Uuid::parse_str(lobby_id).unwrap()).expect("Failed to send message")),
-    //         // Add similar entries for the other events
-    //         _ => Err(format!("Unknown command: {}", input)),
-    //     };
-    //
-    //     print_lobby_state(client.store.lock().unwrap().get_lobby());
-    // }
+    thread::spawn(move || {
+        let stdin = io::stdin();
+        loop {
+            // Read user input
+            let input = get_user_input();
+        
+            let parts: Vec<&str> = input.split_whitespace().collect();
+        
+            let result = match parts.as_slice() {
+                ["broadcast", msg] => Ok(client.send_to_all_clients(msg.to_string()).expect("Failed to broadcast")),
+                ["sendto", recipient, msg] => Ok(client.send_to(Uuid::parse_str(recipient).unwrap(), msg.to_string()).expect("Failed to send message")),
+                ["create_lobby", player_id, lobby_params] => Ok(client.create_lobby(Uuid::parse_str(player_id).unwrap(), serde_json::from_str::<LobbyParams>(lobby_params).unwrap()).expect("Failed to send message")),
+                ["join_lobby", player_id, lobby_id] => Ok(client.join_lobby(Uuid::parse_str(player_id).unwrap(), Uuid::parse_str(lobby_id).unwrap()).expect("Failed to send message")),
+                ["delete_lobby", player_id, lobby_id] => Ok(client.delete_lobby(Uuid::parse_str(player_id).unwrap(), Uuid::parse_str(lobby_id).unwrap()).expect("Failed to send message")),
+                // Add similar entries for the other events
+                _ => Err(format!("Unknown command: {}", input)),
+            };
+            }
+    });
+
 
     loop {
-
+        std::thread::park();
     }
 }
 
